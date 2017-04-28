00009e7be68ed5c3e21a6cb24d70578f83381a87
Aaron Votre <avotre422@gmail.com>
2017-04-28 17:32:01 -0400

    Setup webpack to build into priv dir

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,30 +1,34 @@
 var path = require('path');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: './js/app.js',
+  entry: ['./js/app.js', './css/app.css'],
   output: {
 	filename: 'js/wwm.bundle.js',
 	path: path.resolve(__dirname, '../priv/static'),
 	sourceMapFilename: "wwm.bundle.map",
   },
   module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules|bower_components)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['env']
-					}
-				}
+	rules: [
+		{
+		  test: /\.js$/,
+		  exclude: /(node_modules|bower_components)/,
+		  use: {
+			loader: 'babel-loader',
+			options: {
+	    	  presets: ['es2015']
 			}
-		]
+		  }
+		},
+		{
+		  test: /\.css$/,
+		  loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })
+		}
+	]
 	},
 	plugins: [
-        new CopyWebpackPlugin([
-            { from: 'static' }
-        ])
+      new CopyWebpackPlugin([{ from: './static' }]),
+ 	  new ExtractTextPlugin("css/app.css"),
     ]
 };
